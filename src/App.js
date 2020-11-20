6eb13aabf70ccb227bb5e1a2c99e7508bfc1bf8d
George Tsopozidis <gtsopozidis@gmail.com>
2020-11-21 00:44:18 +0200

    Created three components with HOME ABOUT PROFILE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Profile } from "./Profile";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PAGES = {
+  home: <Home />,
+  about: <About />,
+  profile: <Profile />,
+};
+
+class App extends React.Component {
+  state = { currentPage: "home" };
+
+  navigatTo = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    return (
+      <>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <button
+                  onClick={() => {
+                    this.navigatTo("home");
+                  }}
+                >
+                  Home
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    this.navigatTo("about");
+                  }}
+                >
+                  About
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    this.navigatTo("profile");
+                  }}
+                >
+                  Profile
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section>{PAGES[this.state.currentPage]}</section>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
